595e3cbf2b095d2a7cb761592d4216db33074e2f
busiqiao <busiqiao666@gmail.com>
2023-02-20 22:43:25 +0800

    1）添加公安备案html

--- blog-view/src/components/index/Footer.vue
+++ blog-view/src/components/index/Footer.vue
@@ -1,83 +1,97 @@
 <template>
-	<footer class="ui inverted vertical segment m-padded-tb-large m-opacity">
-		<div class="ui center aligned container">
-			<div class="ui inverted divided stackable grid">
+  <footer class="ui inverted vertical segment m-padded-tb-large m-opacity">
+    <div class="ui center aligned container">
+      <div class="ui inverted divided stackable grid">
 
-				<div class="three wide column">
-					<div class="ui link list">
-						<h4 class="ui inverted header m-text-thin m-text-spaced">{{ siteInfo.footerImgTitle }}</h4>
-						<div class="item">
-							<img :src="siteInfo.footerImgUrl" class="ui rounded image" alt="" style="width: 100px">
-						</div>
-					</div>
-				</div>
+        <div class="three wide column">
+          <div class="ui link list">
+            <h4 class="ui inverted header m-text-thin m-text-spaced">{{ siteInfo.footerImgTitle }}</h4>
+            <div class="item">
+              <img :src="siteInfo.footerImgUrl" class="ui rounded image" alt="" style="width: 100px">
+            </div>
+          </div>
+        </div>
 
-				<div class="six wide column">
-					<h4 class="ui inverted header m-text-thin m-text-spaced">最新博客</h4>
-					<div class="ui inverted link list">
-						<a href="javascript:;" @click.prevent="toBlog(item)" v-for="item in newBlogList" :key="item.id" class="item m-text-thin m-padded-tb-small">{{ item.title }}</a>
-					</div>
-				</div>
+        <div class="six wide column">
+          <h4 class="ui inverted header m-text-thin m-text-spaced">最新博客</h4>
+          <div class="ui inverted link list">
+            <a href="javascript:;" @click.prevent="toBlog(item)" v-for="item in newBlogList" :key="item.id"
+               class="item m-text-thin m-padded-tb-small">{{ item.title }}</a>
+          </div>
+        </div>
 
-				<div class="seven wide column">
-					<p id="hitokotoText" class="m-text-thin m-text-spaced m-opacity-mini">{{ hitokoto.hitokoto }}</p>
-					<p id="hitokotoFrom" class="m-text-thin m-text-spaced m-opacity-mini" style="float: right" v-text="hitokoto.from?`——《${hitokoto.from}》`:''"></p>
-				</div>
-			</div>
+        <div class="seven wide column">
+          <p id="hitokotoText" class="m-text-thin m-text-spaced m-opacity-mini">{{ hitokoto.hitokoto }}</p>
+          <p id="hitokotoFrom" class="m-text-thin m-text-spaced m-opacity-mini" style="float: right"
+             v-text="hitokoto.from?`——《${hitokoto.from}》`:''"></p>
+        </div>
+      </div>
 
-			<div class="ui inverted section divider"></div>
+      <div class="ui inverted section divider"></div>
 
-			<p class="m-text-thin m-text-spaced m-opacity-tiny">
-				<span style="margin-right: 10px" v-if="siteInfo.copyright">{{ siteInfo.copyright.title }}</span>
-				<router-link to="/" style="color:#ffe500" v-if="siteInfo.copyright">{{ siteInfo.copyright.siteName }}</router-link>
-				<span style="margin: 0 15px" v-if="siteInfo.copyright && siteInfo.beian">|</span>
-				<a rel="external nofollow noopener" href="https://beian.miit.gov.cn/" target="_blank" style="color:#ffe500">{{ siteInfo.beian }}</a>
-			</p>
+      <p class="m-text-thin m-text-spaced m-opacity-tiny">
+        <span style="margin-right: 10px" v-if="siteInfo.copyright">{{ siteInfo.copyright.title }}</span>
+        <router-link to="/" style="color:#ffe500" v-if="siteInfo.copyright">{{
+            siteInfo.copyright.siteName
+          }}
+        </router-link>
+        <span style="margin: 0 15px" v-if="siteInfo.copyright && siteInfo.beian">|</span>
+        <a rel="external nofollow noopener" href="https://beian.miit.gov.cn/" target="_blank"
+           style="color:#ffe500">{{ siteInfo.beian }}</a>
+      <div style="width:300px;margin:0 auto;">
+        <img src="/img/beian.png">
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=62050302000331"
+           style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src=""
+                                                                                                style="float:left;"/>
+          <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">甘公网安备
+            62050302000331号</p></a>
+      </div>
+      </p>
 
-			<div class="github-badge" v-for="(item,index) in badges" :key="index">
-				<a rel="external nofollow noopener" :href="item.url" target="_blank" :title="item.title">
-					<span class="badge-subject">{{ item.subject }}</span>
-					<span class="badge-value" :class="`bg-${item.color}`">{{ item.value }}</span>
-				</a>
-			</div>
+      <div class="github-badge" v-for="(item,index) in badges" :key="index">
+        <a rel="external nofollow noopener" :href="item.url" target="_blank" :title="item.title">
+          <span class="badge-subject">{{ item.subject }}</span>
+          <span class="badge-value" :class="`bg-${item.color}`">{{ item.value }}</span>
+        </a>
+      </div>
 
-		</div>
-	</footer>
+    </div>
+  </footer>
 </template>
 
 <script>
-	export default {
-		name: "Footer",
-		props: {
-			siteInfo: {
-				type: Object,
-				required: true
-			},
-			badges: {
-				type: Array,
-				required: true
-			},
-			newBlogList: {
-				type: Array,
-				required: true
-			},
-			hitokoto: {
-				type: Object,
-				required: true
-			}
-		},
-		methods: {
-			toBlog(blog) {
-				this.$store.dispatch('goBlogPage', blog)
-			}
-		}
-	}
+export default {
+  name: "Footer",
+  props: {
+    siteInfo: {
+      type: Object,
+      required: true
+    },
+    badges: {
+      type: Array,
+      required: true
+    },
+    newBlogList: {
+      type: Array,
+      required: true
+    },
+    hitokoto: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    toBlog(blog) {
+      this.$store.dispatch('goBlogPage', blog)
+    }
+  }
+}
 </script>
 
 <style scoped>
-	@import "../../assets/css/badge.css";
+@import "../../assets/css/badge.css";
 
-	.github-badge a {
-		color: #fff;
-	}
+.github-badge a {
+  color: #fff;
+}
 </style>
